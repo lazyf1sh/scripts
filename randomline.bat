a170c0dfc0e730eef7f95d018ccaefde8fd6de04
Ivan K <stealthwar@pm.me>
2019-08-23 17:44:09 +0300

    init

---
+++ randomline.bat
@@ -0,0 +1,14 @@
+@ECHO OFF
+IF "%~1"=="" GOTO :EOF
+IF NOT EXIST %1 GOTO :EOF
+FOR /F "" %%I IN ('FIND /C /V "" ^<%1') DO SET /A lines=%%I
+IF %lines%==0 GOTO :EOF
+SET /A skip=(%RANDOM%*32768+%RANDOM%)%%lines
+<%1 (
+  FOR /L %%I IN (1,1,%skip%) DO (
+    SET /P line=
+  )
+  SET line=
+  SET /P line=
+)
+ECHO(%line%
