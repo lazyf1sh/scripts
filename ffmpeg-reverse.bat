aba260c007f2d50db984d0688b2a21fc79cc9af9
lazyf1sh <f1sh@pm.me>
2020-10-01 11:15:16 +0300

    upd

---
+++ ffmpeg-reverse.bat
@@ -0,0 +1,1 @@
+ffmpeg -i inputfile.mp4 -vf reverse reversed.mp4
--- ffmpeg_make_screenshots.bat
+++ ffmpeg_make_screenshots.bat
@@ -12,7 +12,7 @@ For /R %1 %%a in (*.flv *.mp4 *.mkv) Do (
 Set sourcefile="%%a"
 echo !sourcefile!
 
-ffmpeg -i !sourcefile! -vf fps=1,select="'not(mod(t,30))'" -vsync 0 -frame_pts 1 !sourcefile!_%%4d.jpg
+ffmpeg -i !sourcefile! -vf fps=1,select="'not(mod(t,5))'" -vsync 0 -frame_pts 1 !sourcefile!_%%4d.jpg
 )
 @PAUSE
 
