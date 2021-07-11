8b28282430d26377d3a52b7e6ced3b121355660e
Ivan <stealthwar@pm.me>
2021-07-11 09:45:45 +0300

    upd

--- kill_attention_sink/kill_attention_sink_starter.bat
+++ kill_attention_sink/kill_attention_sink_starter.bat
@@ -1,3 +1,5 @@
 cd %~dp0
+timeout 10
 kill_attention_sink_hidden.vbs kill_attention_sink_worker.bat
+timeout 10
 
--- kill_attention_sink/kill_attention_sink_worker.bat
+++ kill_attention_sink/kill_attention_sink_worker.bat
@@ -1,6 +1,6 @@
 :loop_label
 
-timeout 60
+timeout 180
 taskkill /f /im telegram.exe
 
 goto loop_label
